182f503101700cb411b7c4a3dff6d7a95158c5a2
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-06-30 22:22:42 +0530

    made some files

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-// this is like the back end, db etc. 
+// this is like the back end, db etc.
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
 
-const { app, BrowserWindow } = require('electron')
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "development";
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // webPreferences: {
-        //     nodeIntegration: true
-        // }
-    })
+	const mainWindow = new BrowserWindow({
+		width: 900,
+		height: 700,
+	});
 
-    win.loadFile('index.html')
+	if (isDev) {
+		mainWindow.webContents.openDevTools();
+	}
+
+	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+	createWindow();
 
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on(!isMac ? "window-all-closed" : "before-quit", () => {
+	if (!isMac) {
+		app.quit();
+	}
+});
